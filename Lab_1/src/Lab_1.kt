9a7d43557558921c3da2433597dce1279e3fb7e5
Kondratenko Danna <72597800+KondratenkoDanna@users.noreply.github.com>
2021-04-14 12:31:21 +0300

    Task 7.

--- Lab_1/src/Lab_1.kt
+++ Lab_1/src/Lab_1.kt
@@ -1,10 +1,34 @@
-    fun main() {
-        var x: Int = readLine()!!.toInt()
-        var sum = 0
-        while (x != 0)
-        {
-            sum += x % 10
-            x /= 10
-        }
-        println(sum)
+fun sumNumber(x: Int) : Int
+{
+    var n = x
+    var sum = 0
+    while (n != 0)
+    {
+        sum += n % 10
+        n /= 10
     }
+    return sum
+}
+fun prNumber(x: Int) : Int
+{
+    var n = x
+    var composition = 0
+    while (n != 0)
+    {
+        composition *= n % 10
+        n /= 10
+    }
+    return composition
+}
+
+fun max(num:Int): Int = if (num < 10) num else if (num % 10 > max(num/10)) num % 10 else max(num/10)
+
+fun min(num:Int): Int = if (num < 10) num else if (num % 10 < min(num/10)) num % 10 else min(num/10)
+
+fun main() {
+    val sum: Int = readLine()!!.toInt()
+    val max: Int = max(sum)
+    val min: Int = min(sum)
+    println("max = $max")
+    println("min = $min")
+}
