8be0de10935f67ed8b4e01742f815514b253fc4c
Kondratenko Danna <72597800+KondratenkoDanna@users.noreply.github.com>
2021-04-16 23:27:15 +0300

    Task 2.

--- Новая папка/src/Lab_2.kt
+++ Новая папка/src/Lab_2.kt
@@ -1,6 +1,9 @@
 fun sumNumberUp(x: Int): Int = if (x == 0) x else sumNumberUp(x/10) + x%10
 
+fun sumDown(num: Int): Int = sumDown(num, 0)
+tailrec fun sumDown(num: Int, sum: Int): Int = if(num == 0) sum else sumDown(num/10,sum+num%10)
+
 fun main() {
     val num: Int = readLine()!!.toInt()
-    println(sumNumberUp(num))
+    println(sumDown(num))
 }
