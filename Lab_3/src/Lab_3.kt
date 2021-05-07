09229574db59cbe0dc71efb8cc1cf5d6166780da
Kondratenko Danna <72597800+KondratenkoDanna@users.noreply.github.com>
2021-05-07 10:52:50 +0300

    Task 1.

--- Lab_3/src/Lab_3.kt
+++ Lab_3/src/Lab_3.kt
@@ -1,27 +1,30 @@
 fun main()
 {
     val n = readLine()!!.toInt()
-    val ar: Array<Int?> = arrayOfNulls<Int>(n)
+    var ar: Array<Int> = Array<Int>(n,{0})
 
-    val array: Array<Int> = emptyArray()
-    arrayInput(array,n)
+    arrayInput(ar,n)
     ar.forEach{
         print("$it ")
     }
-    println(min(ar as Array<Int>, 9, n))
+    /*println(ar.min())
+    println(ar.max())
+    println(ar.sum())
+    println(ar.fold(1, {currentResult, element -> currentResult * element}))*/
+    println("умножение = ${arrayOp(ar.iterator(), {a,b -> a*b}, ar[0])}")
+
 }
 
-fun arrayInput(array : Array<Int>, size : Int) : Array<Int> =
+fun arrayInput(array : Array<Int>, size : Int) : Array<Int> =   // ввод массива с клавиатуры
     arrayInput(array, 0, size)
-
 tailrec fun arrayInput(array : Array<Int>, counter : Int, size : Int) : Array<Int> =
     if (counter == size) array else {
         array[counter] = readLine()!!.toInt()
         arrayInput(array, counter + 1, size)
     }
 
-fun min(ar : Array<Int>, min: Int, count: Int) : Int =
-    if (ar.size == count) min else
-        if (ar[count] < min) min(ar, ar[count], count + 1) else
-            min(ar, min, count + 1)
+tailrec fun arrayOp(a: Iterator<Int>, f: (Int, Int) -> Int, result: Int): Int =
+    if (a.iterator().hasNext() == false) result else
+        arrayOp(a, f, f(a.iterator().next(),result))
+
 
